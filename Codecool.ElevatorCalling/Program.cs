b912da4805f1f407a2699c8898d529548635036d
appuser <appuser@journey2-679f96f5bd-jw48d>
2024-06-19 13:10:23 +0000

    starter project

---
+++ Codecool.ElevatorCalling/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Codecool.ElevatorCalling;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Incoming requests:
+        //
+        // Outside UP request from floor 0
+        // Inside UP request going to floor 3.
+        // Inside UP request going to floor 2.
+        // Outside DOWN request from floor 4.
+        // Outside DOWN request from floor 1.
+        // Inside DOWN request going to floor 1.
+        // Inside DOWN request going to floor 2.
+        // Inside DOWN request going to floor 0.
+    }
+}
